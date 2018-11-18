31ffd1746e5bd8d4f8c3f2141aec9ab66eb88335
Judson Stevens <Judson.Stevens@gmail.com>
2018-11-17 21:13:33 -0700

    Added correct namespacing for custom error class

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,12 @@
 class ApiController < ActionController::API
   include Knock::Authenticable
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from PlanItChange::Error::UserIsNotAdmin, with: :render_error_response
+  
   protected
-
+  
   def current_user
     @current_user ||= begin
       Knock::AuthToken.new(token: token).entity_for(User)
@@ -10,4 +14,24 @@ class ApiController < ActionController::API
       nil
     end
   end
+  
+  def authorize_as_admin
+    raise PlanItChange::Error::UserIsNotAdmin.new unless current_user.is_admin?
+  end
+  
+  def render_unprocessable_entity_response(exception)
+    render json: {
+      message: "Validation Failed",
+      errors: ValidationErrorsSerializer.new(exception.record).serialize
+      }, status: :unprocessable_entity
+  end
+    
+  def render_not_found_response
+    render json: { message: "Not found", code: "not_found"}, status: :not_found
+  end
+  
+  def render_error_response(exception)
+    render json: { message: exception.message, code: exception.code}, status: exception.http_status
+  end
+    
 end
---
+++ lib/plan_it_change/error/user_is_not_admin.rb
@@ -0,0 +1,26 @@
+module PlanItChange
+  module Error
+        
+    class UserIsNotAdmin < StandardError
+
+      def http_status
+        403
+      end
+
+      def code
+        'not_an_admin'
+      end
+
+      def message
+        "User doesn't have sufficient rights"
+      end
+
+      def to_hash
+        {
+          message: message,
+          code: code
+        }
+      end
+    end
+  end
+end
