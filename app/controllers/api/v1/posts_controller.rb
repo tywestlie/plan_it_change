38d00d5d11d59a6d2a778d825b6b1bd5d291ce5d
Judson Stevens <Judson.Stevens@gmail.com>
2018-11-21 07:58:44 -0700

    Added posts and comments controller, model and migrations

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::PostsController < ApiController
+  def index
+
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def post_params
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_001403) do
+ActiveRecord::Schema.define(version: 2018_11_21_010126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 2018_11_19_001403) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.bigint "posts_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["posts_id"], name: "index_comments_on_posts_id"
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "city"
     t.string "state"
@@ -60,6 +68,14 @@ ActiveRecord::Schema.define(version: 2018_11_19_001403) do
     t.bigint "event_id", null: false
     t.index ["event_id"], name: "index_events_users_on_event_id"
     t.index ["user_id"], name: "index_events_users_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
