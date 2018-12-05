188e6dbd3286f4b4488555289b75d5cf9a9ed367
Tyler Westlie <tyler.westlie@gmail.com>
2018-12-05 15:42:39 -0700

    adds tests and factory for posts

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::PostsController, type: :controller do
 
+  describe "Get #index" do
+    it "retreives all posts by location" do
+      get :index
+    end
+  end
+
 end
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :post do
-    
+    user        { Faker::Name.unique.first_name }
+    body        { Faker::HitchhikersGuideToTheGalaxy.quote }
   end
 end
